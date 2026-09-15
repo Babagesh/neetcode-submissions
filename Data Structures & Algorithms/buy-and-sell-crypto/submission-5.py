65aa700006441abb0c1cc7f5b055db833f876754
Bhagyesh Jethwani <127056352+Babagesh@users.noreply.github.com>
2026-09-15 00:40:04 -0700

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # We keep a left pointer which tracks what we would buy a stock at
+        # We have a right which represents when we would sell a stock
+        # We see current profit status
+        # If left pointer greater, we increment left since we want to buy at cheaper price
+        # if right pointer greater or equal, We increment right until it keeps increasing, updating max profit found
+        # once a decrease is found we stop
+        # we move left to right, and right to left + 1
+        # then we repeat
+
+
+        # Lets say array size of 2
+        # if the array is [10, 1] we increase left to 1 and then compare prices, see that equal, increment right until end, end is found, and then return the profit which is 0
+
+        # For array of size 1 profit is just 0
+        if len(prices) <= 1:
+            return 0
+        left = 0
+        right = left + 1
+        max_profit = 0
+        while left < len(prices) and right < len(prices):
+            buy_price = prices[left]
+            sell_price = prices[right]
+            if buy_price < sell_price:
+                while right < len(prices) and prices[right] >= buy_price:
+                    profit = prices[right] - buy_price
+                    if profit > max_profit:
+                        max_profit = profit
+                    right += 1
+            else:
+                left = right
+                right += 1
+        return max_profit
+
